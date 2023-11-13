a81a84ff3553167e94c00f5f73b3f8633cd199e5
Andrew Hong <andrew.jwn.hong@gmail.com>
2023-11-12 23:37:22 -0500

    Many deletions

--- docs/1.7/classtainicom_1_1_aether_1_1_physics2_d_1_1_content_1_1_polygon_container_reader.js
+++
@@ -1,4 +0,0 @@
-var classtainicom_1_1_aether_1_1_physics2_d_1_1_content_1_1_polygon_container_reader =
-[
-    [ "Read", "classtainicom_1_1_aether_1_1_physics2_d_1_1_content_1_1_polygon_container_reader.html#a4146224abddea5cfdce4065e4ec7aeac", null ]
-];
